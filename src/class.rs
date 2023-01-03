70ddef4c8af1f380ee43aeae470846266fa1df90
ryanloftus <ryanloftus02@outlook.com>
2023-01-03 17:30:43 -0500

    adding classes to class prompt and fixing issues with consts

--- src/class.rs
+++ src/class.rs
@@ -1,55 +1,41 @@
+use std::borrow::Cow;
+
 #[derive(Debug, Clone)]
 pub struct Class {
-    pub name: String,
-    pub unique_id: String,
+    pub name: Cow<'static, str>,
+    pub unique_id: Cow<'static, str>,
     // TODO: add stat modifiers and skills and class progressions
 }
 
 impl Class {
-    fn to_string(&self) -> String {
+    pub fn to_string(&self) -> String {
         format!("({}) {}", self.unique_id, self.name)
+    }
+
+    const fn new_const(name: &'static str, unique_id: &'static str) -> Self {
+        Self {
+            name: Cow::Borrowed(name),
+            unique_id: Cow::Borrowed(unique_id),
+        }
     }
 }
 
 mod starter {
     use super::Class;
 
-    pub const swordsman: Class = Class {
-        name: "Swordsman".to_string(),
-        unique_id: "S".to_string(),
-    };
-
-    pub const knight: Class = Class {
-        name: "Knight".to_string(),
-        unique_id: "K".to_string(),
-    };
-
-    pub const brawler: Class = Class {
-        name: "Brawler".to_string(),
-        unique_id: "B".to_string(),
-    };
-
-    pub const mage: Class = Class {
-        name: "Mage".to_string(),
-        unique_id: "M".to_string(),
-    };
-
-    pub const healer: Class = Class {
-        name: "Healer".to_string(),
-        unique_id: "H".to_string(),
-    };
-
-    pub const gambler: Class = Class {
-        name: "Gambler".to_string(),
-        unique_id: "G".to_string(),
-    };
+    pub const SWORDSMAN: Class = Class::new_const("Swordsman", "S");
+    pub const KNIGHT: Class = Class::new_const("Knight", "K");
+    pub const BRAWLER: Class = Class::new_const("Brawler", "B");
+    pub const MAGE: Class = Class::new_const("Mage", "M");
+    pub const HEALER: Class = Class::new_const("Healer", "H");
+    pub const GAMBLER: Class = Class::new_const("Gambler", "G");
 }
 
-pub const starter_classes: [Class; 6] = [
-    starter::swordsman,
-    starter::knight,
-    starter::brawler,
-    starter::mage,
-    starter::healer,
-    starter::gambler,
+pub const STARTER_CLASSES: [Class; 6] = [
+    starter::SWORDSMAN,
+    starter::KNIGHT,
+    starter::BRAWLER,
+    starter::MAGE,
+    starter::HEALER,
+    starter::GAMBLER,
 ];
--- src/prompts/class_prompt.rs
+++ src/prompts/class_prompt.rs
@@ -1,29 +1,42 @@
-use crate::utils::io_util;
 use crate::class;
+use crate::utils::io_util;
 
 // a class name is valid only if it is equal to a class' unique_id or name
 fn is_valid_class(class: String, options: &[class::Class]) -> bool {
     if class.len() == 1 {
-        options.iter().any(|real_class| class == real_class.unique_id)
+        options
+            .iter()
+            .any(|real_class| class == real_class.unique_id)
     } else {
-        options.iter().any(|real_class| class == real_class.name)
+        options
+            .iter()
+            .any(|real_class| class == real_class.name)
     }
 }
 
 fn is_valid_starter_class(class: String) -> bool {
-    is_valid_class(class, &class::starter_classes)
+    is_valid_class(class, &class::STARTER_CLASSES)
 }
 
 fn starter_class_from_string(class: String) -> class::Class {
     if class.len() == 1 {
-        class::starter_classes.iter().find(|real_class| class == real_class.unique_id).expect("").clone()
+        class::STARTER_CLASSES
+            .iter()
+            .find(|real_class| class == real_class.unique_id)
+            .expect("")
+            .clone()
     } else {
-        class::starter_classes.iter().find(|real_class| class == real_class.name).expect("").clone()
+        class::STARTER_CLASSES
+            .iter()
+            .find(|real_class| class == real_class.name)
+            .expect("")
+            .clone()
     }
 }
 
 pub fn starting_class_prompt() -> class::Class {
     println!("What class of fighter are you?");
+    class::STARTER_CLASSES.iter().for_each(|class| println!("{}", class.to_string()));
     let class = io_util::request_input(
         "Choose a class.",
         is_valid_starter_class,
