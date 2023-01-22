e695b1d5524d6133987622d9a85355bc2b5c2a7c
ryanloftus <ryanloftus02@outlook.com>
2023-01-22 14:21:56 -0500

    fixing serialization error with skills

--- src/skill/attribute.rs
+++ src/skill/attribute.rs
@@ -1,20 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+use crate::prompt::PromptOption;
+
 /*
  * SkillAttributes describe a Skill and allow Skills to have more complex effects than simply dealing damage
  */
-pub type SkillAttribute = &'static str;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SkillAttribute {
+    Ranged,
+    Melee,
+    Magic,
+    Physical,
+    Hot,
+    Cold,
+    Electric,
+    Dark,
+    Light,
+    Healing,
+    Defensive,
+    DamageOverTime,
+    AreaOfEffect,
+    ArmorPiercing,
+    MagicResistancePiercing,
+}
 
-pub const RANGED: SkillAttribute = "Ranged";
-pub const MELEE: SkillAttribute = "Melee";
-pub const MAGIC: SkillAttribute = "Magic";
-pub const PHYSICAL: SkillAttribute = "Physical";
-pub const HOT: SkillAttribute = "Hot";
-pub const COLD: SkillAttribute = "Cold";
-pub const ELECTRIC: SkillAttribute = "Electric";
-pub const DARK: SkillAttribute = "Dark";
-pub const LIGHT: SkillAttribute = "Light";
-pub const HEALING: SkillAttribute = "Healing";
-pub const DEFENSIVE: SkillAttribute = "Defensive";
-pub const DOT: SkillAttribute = "Damage Over Time";
-pub const AOE: SkillAttribute = "Area of Effect";
-pub const ARMOR_PEN: SkillAttribute = "Armor Piercing";
-pub const MAGIC_PEN: SkillAttribute = "Magic Piercing";
+impl PromptOption for SkillAttribute {
+    fn option_name(&self) -> String {
+        todo!()
+    }
+
+    fn short_option_name(&self) -> Option<String> {
+        todo!()
+    }
+}
